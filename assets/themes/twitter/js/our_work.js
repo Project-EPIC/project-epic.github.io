7a99148eb13d0bd3353677ef4c0053b7d62ced0f
Mario Barrenechea <mario.barrenechea@colorado.edu>
2013-12-18 14:03:19 -0500

    Committing sample parallax effect under "Our Work". Check it out, let me know what you think of it. I want to add JQuery Waypoints to allow execution of event functions when specified HTML element is scrolled by.

--- assets/themes/twitter/js/our_work.js
+++ assets/themes/twitter/js/our_work.js
@@ -1,13 +1,18 @@
 $(document).ready(function(){
 
-	$(window).stellar({
+	$('section[data-type="background"]').each(function(){
+		var $bgobj = $(this); // assigning the object
 
-		verticalScrolling:true,
-		verticalOffset: 150,
-		responsive: false,
-		scrollProperty: 'scroll'
-	});
+		$(window).scroll(function(){
+			var yPos = -($(window).scrollTop() / $bgobj.data('speed'));
 
-
+			// Put together our final background position
+			var coords = '50% '+ yPos + 'px';
+			 
+			// Move the background
+			$bgobj.css({ backgroundPosition: coords });			
+	    
+		});       
+    });
 
 });
