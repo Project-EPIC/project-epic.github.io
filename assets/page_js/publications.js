8807c518da4099d222eccca4c03afd4f650e6577
Jennings Anderson <jennings.anderson@colorado.edu>
2014-01-26 16:26:24 -0700

    this is my update

--- assets/page_js/publications.js
+++ assets/page_js/publications.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
   $(".publication-expand").hide();
   //toggle the componenet with class msg_body
-  $(".publication-item").click(function()
-  {
-    $(this).next(".publication-expand").slideToggle(500);
+  $(".publication-item").click(function(){
+    $(this).next(".publication-expand").slideToggle(400);
+    $('html,body').animate({
+   		scrollTop: $(this).next(".publication-expand").offset().top -100
+	});
   });
 });
