5a7463fa81d40826e8b740427bb0e34972c87f36
Jennings Anderson <jennings.anderson@colorado.edu>
2014-09-01 15:25:28 -0600

    updated the publications page

--- assets/page_js/publications.js
+++ assets/page_js/publications.js
@@ -1,12 +1,23 @@
 $(document).ready(function() {
-  $("#scrolling-nav-div").hide()
   $(".publication-expand").hide();
   //toggle the componenet with class msg_body
+  
   $(".publication-item").click(function(){
-  	$(this).toggleClass('active-publication')
-    $(this).next(".publication-expand").slideToggle(400);
-    $('html,body').animate({
-   		scrollTop: $(this).next(".publication-expand").offset().top -100
-	});
+    if ($(this).next(".publication-expand").is(":visible")){
+      //If it is visible, then remove the class & hide it
+      $(this).removeClass('active-publication')
+      $(this).next(".publication-expand").slideToggle(400);
+    }
+    else{
+      //Hide all previous & remove the active class
+      $(".publication-item").removeClass('active-publication')
+      $(".publication-expand").hide(400);
+      
+      //Add active class to this and then show the slide
+      $(this).addClass('active-publication')
+      $(this).next(".publication-expand").slideToggle(400);
+    }
+
   });
+
 });
