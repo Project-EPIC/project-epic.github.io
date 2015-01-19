f0e8d855eec0b8c65cc0ff827c21fd452a53ecb7
Jennings Anderson <jennings.anderson@colorado.edu>
2015-01-19 11:50:31 -0700

    cleaning up the scorlling nav

--- assets/page_js/publications.js
+++ assets/page_js/publications.js
@@ -1,8 +1,34 @@
 $(document).ready(function() {
-  $(".scrolling-nav-list").hide();
+
+  //Be sure to hide all the descriptions
   $(".publication-expand").hide();
-  //toggle the componenet with class msg_body
   
+  //Hide the subnav
+  $("#scrolling-nav-div").hide()
+
+  //Assign a Waypoint to it
+  $("#scrolling-nav-div").waypoint(function() {
+    $("#scrolling-nav-div").show(1000);
+  }, { offset: 0});
+
+
+  //Now add the logic to each element in the list:
+  $(".sub-nav-list-item").each(function( index ) {
+      
+      var link = $('a',this).attr('href');
+      
+      $(link).waypoint(function(index){
+        $(".sub-nav-list-item").removeClass("active")
+        $(this).toggleClass('active')
+      },{offset: 100})
+
+      $(this).click(function(){
+        $('html,body').animate({scrollTop: $(link).offset().top-150}, 'medium');
+      })      
+    });
+
+
+  //Handle the hiding / showing of abstracts
   $(".publication-item").click(function(){
     if ($(this).next(".publication-expand").is(":visible")){
       //If it is visible, then remove the class & hide it
